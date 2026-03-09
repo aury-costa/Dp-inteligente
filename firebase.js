b68370bceed2822c2a817c273ef79e1c53e5e73b
aury-costa <aury.costa@delmoro.com.br>
2026-03-09 07:15:10 -0400

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-import { getDatabase, ref, push, set, update, onValue, get, serverTimestamp, query, orderByChild, limitToLast } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+import { getDatabase, ref, push, set, update, onValue, get } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-storage.js";
 import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getDatabase(app);
+const db = getDatabase(app, firebaseConfig.databaseURL);
 const storage = getStorage(app);
 
 export {
@@ -19,10 +19,6 @@ export {
   update,
   onValue,
   get,
-  serverTimestamp,
-  query,
-  orderByChild,
-  limitToLast,
   storageRef,
   uploadBytes,
   getDownloadURL,
